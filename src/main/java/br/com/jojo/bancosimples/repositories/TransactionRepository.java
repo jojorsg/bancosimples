5ece5e7a8830063bb793e0fdd8a2c32888bb8282
jojorsg <josueroberto099@gmail.com>
2023-11-13 17:23:22 -0300

    Implementando repositório da Transaction

--- src/main/java/br/com/jojo/bancosimples/repositories/TransactionRepository.java
+++ src/main/java/br/com/jojo/bancosimples/repositories/TransactionRepository.java
@@ -3,5 +3,5 @@ package br.com.jojo.bancosimples.repositories;
 import br.com.jojo.bancosimples.domain.transaction.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TransacionRepository extends JpaRepository<Transaction, Long> {
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 }
