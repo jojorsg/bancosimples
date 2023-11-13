1e5429b0e57cdf9d7cb7ca788169f4ffbb3c52c9
jojorsg <josueroberto099@gmail.com>
2023-11-13 16:38:37 -0300

    Initial commit

---
+++ src/main/java/br/com/jojo/bancosimples/BancosimplesApplication.java
@@ -0,0 +1,13 @@
+package br.com.jojo.bancosimples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancosimplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancosimplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jojo/bancosimples/BancosimplesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jojo.bancosimples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancosimplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
