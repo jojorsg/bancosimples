74b2a4a4cf89fb440e3eaa43e0b9789b521bf51e
jojorsg <josueroberto099@gmail.com>
2023-11-14 16:13:35 -0300

    Implementando classes Services

---
+++ src/main/java/br/com/jojo/bancosimples/services/TransactionService.java
@@ -0,0 +1,63 @@
+package br.com.jojo.bancosimples.services;
+
+import br.com.jojo.bancosimples.domain.transaction.Transaction;
+import br.com.jojo.bancosimples.domain.user.User;
+import br.com.jojo.bancosimples.dtos.TransactionDTO;
+import br.com.jojo.bancosimples.repositories.TransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Service
+public class TransactionService {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private TransactionRepository repository;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public void createTransaction (TransactionDTO transaction) throws Exception {
+        User sender = userService.findUserById(transaction.senderId());
+        User receiver = userService.findUserById(transaction.receiverId());
+
+        userService.validateTransaction(sender, transaction.value());
+
+        boolean isAuthorized = authorizeTransaction(sender, transaction.value());
+        if(!isAuthorized) {
+            throw new Exception("Transação não autorizada");
+        }
+
+        Transaction transaction1 = new Transaction();
+        transaction1.setAmount(transaction.getAmount);
+        transaction1.setSender(sender);
+        transaction1.setReceiver(receiver);
+        transaction1.setTimestamp(LocalDateTime.now());
+
+        sender.setBalance(sender.getBalance().subtract(transaction.value()));
+        receiver.setBalance(receiver.getBalance().add(transaction.value()));
+
+        repository.save(transaction1);
+        userService.saveUser(sender);
+        userService.saveUser(receiver);
+    }
+
+    public boolean authorizeTransaction(User sender, BigDecimal value) {
+        ResponseEntity<Map> authorizationResponse = restTemplate.getForEntity(
+                "https://run.mocky.io/v3/8fafdd68-a090-496f-8c9a-3442cf30dae6", Map.class);
+
+        if(authorizationResponse.getStatusCode() == HttpStatus.OK) {
+            String message = (String) authorizationResponse.getBody().get("message");
+            return "Autorizado".equalsIgnoreCase(message);
+        } else return false;
+    }
+}
