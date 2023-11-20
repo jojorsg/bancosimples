31a105d728e66824e7de333280f35aaafd9bb28a
jojorsg <josueroberto099@gmail.com>
2023-11-20 15:02:49 -0300

    Adicionando métodos para o controller

--- src/main/java/br/com/jojo/bancosimples/services/UserService.java
+++ src/main/java/br/com/jojo/bancosimples/services/UserService.java
@@ -2,11 +2,14 @@ package br.com.jojo.bancosimples.services;
 
 import br.com.jojo.bancosimples.domain.user.User;
 import br.com.jojo.bancosimples.domain.user.UserType;
+import br.com.jojo.bancosimples.dtos.UserDTO;
 import br.com.jojo.bancosimples.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Service
 public class UserService {
@@ -29,5 +32,15 @@ public class UserService {
 
     public void saveUser(User user) {
         repository.save(user);
+    }
+
+    public User createUser(UserDTO data) {
+        User newUser = new User(data);
+        saveUser(newUser);
+        return newUser;
+    }
+
+    public List<User> getAllUsers() {
+        return repository.findAll();
     }
 }
